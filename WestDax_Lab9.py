1ca6ef8916084c3038efaf13e37967988a3c0af4
DaxWest <awest49@uwo.ca>
2024-11-13 14:50:42 -0500

    rearranging variables and initializing the function

--- WestDax_Lab9.py
+++ WestDax_Lab9.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # * Select numerical parameters (time step, grid spacing, etc.).
-method = input('Choose a numerical method, 1) FTCS; 2) Lax; 3) Lax-Wendroff :')
+method = input('Choose a numerical method by entering the corresponding integer, 1) FTCS; 2) Lax; 3) Lax-Wendroff :')
 N = int(input('Enter the number of grid points: '))
 L = 400.  # System size (meters)
 h = L / N  # Grid spacing for periodic boundary conditions
@@ -15,58 +15,61 @@ print('Suggested timestep is ', h / v_max)
 tau = float(input('Enter time step (tau): '))
 print('Last car starts moving after ', (L / 4) / (v_max * tau), 'steps')
 nstep = int(input('Enter number of steps: '))
-coeff = tau / (2 * h)  # Coefficient used by all schemes
-coefflw = tau ** 2 / (2 * h ** 2)  # Coefficient used by Lax-Wendroff
-
-# * Set initial and boundary conditions
 rho_max = 1.0  # Maximum density
-Flow_max = 0.25 * rho_max * v_max  # Maximum Flow
-Flow = np.empty(N)
-cp = np.empty(N);
-cm = np.empty(N)
-# Initial condition is a square pulse from x = -L/4 to x = 0
-rho = np.zeros(N)
-for i in range(int(N / 4), int(N / 2)):
-    rho[i] = rho_max  # Max density in the square pulse
 
-rho[int(N / 2)] = rho_max / 2  # Try running without this line
+def eqn_advection(method, N, h, tau, v_max, rho_max):
+    coeff = tau / (2 * h)  # Coefficient used by all schemes
+    coefflw = tau ** 2 / (2 * h ** 2)  # Coefficient used by Lax-Wendroff
 
-# Use periodic boundary conditions
-ip = np.arange(N) + 1
-ip[N - 1] = 0  # ip = i+1 with periodic b.c.
-im = np.arange(N) - 1
-im[0] = N - 1  # im = i-1 with periodic b.c.
+    # * Set initial and boundary conditions
+    Flow_max = 0.25 * rho_max * v_max  # Maximum Flow
+    Flow = np.empty(N)
+    cp = np.empty(N)
+    cm = np.empty(N)
+    # Initial condition is a square pulse from x = -L/4 to x = 0
+    rho = np.zeros(N)
+    for i in range(int(N / 4), int(N / 2)):
+        rho[i] = rho_max  # Max density in the square pulse
 
-# * Initialize plotting variables.
-iplot = 1
-xplot = (np.arange(N) - 1 / 2.) * h - L / 2.  # Record x scale for plot
-rplot = np.empty((N, nstep + 1))
-tplot = np.empty(nstep + 1)
-rplot[:, 0] = np.copy(rho)  # Record the initial state
-tplot[0] = 0  # Record the initial time (t=0)
+    rho[int(N / 2)] = rho_max / 2  # Try running without this line
 
-# * Loop over desired number of steps.
-for istep in range(nstep):
+    # Use periodic boundary conditions
+    ip = np.arange(N) + 1
+    ip[N - 1] = 0  # ip = i+1 with periodic b.c.
+    im = np.arange(N) - 1
+    im[0] = N - 1  # im = i-1 with periodic b.c.
 
-    # * Compute the flow = (Density)*(Velocity)
-    Flow[:] = rho[:] * (v_max * (1 - rho[:] / rho_max))
+    # * Initialize plotting variables.
+    iplot = 1
+    xplot = (np.arange(N) - 1 / 2.) * h - L / 2.  # Record x scale for plot
+    rplot = np.empty((N, nstep + 1))
+    tplot = np.empty(nstep + 1)
+    rplot[:, 0] = np.copy(rho)  # Record the initial state
+    tplot[0] = 0  # Record the initial time (t=0)
 
-    # * Compute new values of density using
-    #  FTCS, Lax or Lax-Wendroff method.
-    if method == 1:  ### FTCS method ###
-        rho[:] = rho[:] - coeff * (Flow[ip] - Flow[im])
-    elif method == 2:  ### Lax method ###
-        rho[:] = .5 * (rho[ip] + rho[im]) - coeff * (Flow[ip] - Flow[im])
-    else:  ### Lax-Wendroff method ###
-        cp[:] = v_max * (1 - (rho[ip] + rho[:]) / rho_max);
-        cm[:] = v_max * (1 - (rho[:] + rho[im]) / rho_max);
-        rho[:] = rho[:] - coeff * (Flow[ip] - Flow[im]) + coefflw * (
-                cp[:] * (Flow[ip] - Flow[:]) - cm[:] * (Flow[:] - Flow[im]))
+    # * Loop over desired number of steps.
+    for istep in range(nstep):
 
-    # * Record density for plotting.
-    rplot[:, iplot] = np.copy(rho)
-    tplot[iplot] = tau * (istep + 1)
-    iplot += 1
+        # * Compute the flow = (Density)*(Velocity)
+        Flow[:] = rho[:] * (v_max * (1 - rho[:] / rho_max))
+
+        # * Compute new values of density using
+        #  FTCS, Lax or Lax-Wendroff method.
+        if method == 1:  ### FTCS method ###
+            rho[:] = rho[:] - coeff * (Flow[ip] - Flow[im])
+        elif method == 2:  ### Lax method ###
+            rho[:] = .5 * (rho[ip] + rho[im]) - coeff * (Flow[ip] - Flow[im])
+        else:  ### Lax-Wendroff method ###
+            cp[:] = v_max * (1 - (rho[ip] + rho[:]) / rho_max);
+            cm[:] = v_max * (1 - (rho[:] + rho[im]) / rho_max);
+            rho[:] = rho[:] - coeff * (Flow[ip] - Flow[im]) + coefflw * (
+                    cp[:] * (Flow[ip] - Flow[:]) - cm[:] * (Flow[:] - Flow[im]))
+
+        # * Record density for plotting.
+        rplot[:, iplot] = np.copy(rho)
+        tplot[iplot] = tau * (istep + 1)
+        iplot += 1
+    return
 
 # * Graph density versus position and time as wire-mesh plot
 from matplotlib import cm
